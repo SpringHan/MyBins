433b0b9f060a3ae8f35537f05e4ce5c5a2a00d38
SpringHan <3379184879@qq.com>
2020-03-10 15:21:37 +0800

    Moved and added

--- picture_refresh.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-while true; do
-	/bin/bash ~/MyBins/picture.sh
-	sleep 5m
-done
---
+++ setup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+ln -sf ~/MyBins/picture ~/picture
+ln -sf ~/MyBins/picture_dark ~/picture_dark
