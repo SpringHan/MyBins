24d6950feaef25f6ba5394f7338484d399e1558e
SpringHan <3379184879@qq.com>
2020-04-20 18:53:13 +0800

    Modified a file

--- screenphoto.sh
+++ screenphoto.sh
@@ -5,11 +5,11 @@ i=1
 
 while [ ${i} == 1 ]
 do
-	if [ -e "/home/spring/"${a}".png" ]
+	if [ -e "/home/spring/Pictures/"${a}".png" ]
 	then
 		a=`expr ${a} + 1`
 	else
-		scrot -d 1 -q 90 -o /home/spring/${a}.png
+		scrot -d 1 -q 90 -o /home/spring/Pictures/${a}.png
 		i=0
 	fi
 done
